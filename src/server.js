45ccea4615e22320f35d46a7375e5a05e60e2f5a
ini-rahasia <inirahasia6@gmail.com>
2021-05-22 23:24:55 +0700

    add some info to package.json

--- src/server.js
+++ src/server.js
@@ -5,6 +5,9 @@ const init = async () => {
   const server = Hopi.server({
     port: 5000,
     // host: process.env.NODE_ENV !== 'production' ? 'localhost' : 'IP Public EC2 Instance',
+    // and add property scripts in package.json with this two values:
+    // "start-prod": "NODE_ENV=production node ./src/server.js",
+    // "start-dev": "nodemon ./src/server.js",
     host: 'localhost',
     routes: {
       cors: {
